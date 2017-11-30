9dceb9710df3f1809f1d967c6427499abb53234b
10203040506070809 <33032000+10203040506070809@users.noreply.github.com>
2017-11-30 15:49:54 +0000

    added some coursework ideas

---
+++ AINT151 Coursework/`/data.js
@@ -0,0 +1,81 @@
+var roomArray = [
+	{
+
+		//room 0
+		title:"Cryogenics Room",
+		text:"You are standing in a large rectangular room, eastern and western walls embedded with bulbous green pods.There's a strange liquid covering most of the floor, which seems to be leading towards the pods on the opposite wall. There are two doors on the southern and northern walls respectively. A pungent smell emanates from behind you. ",
+		choices:[
+			{
+				text:"Try the southern door.",
+				index:1
+			},
+			{
+				text:"Try the northern door.",
+				index:2
+			}
+		]
+	},
+	{
+		//room 1
+		title:'Corridor 1',
+		text:"There's a long grey corridor ahead of you, with two doors on the western side.The first door has a plaque that says, 'Batch 3-47B'. The second appears to have been torn apart.   A thin window traverses the length of thae eastern wall, outside of which you can see the expanses of space. A lone blue planet is visible in the distance.  ",
+		choices:[
+			{
+				text:"Go back to the Cryogenics Room",
+				index:0
+			},
+			{
+				text:"Enter the door labelled, 'Batch 3-47B'.",
+				index:3
+			},
+			{
+				text:"Crawl through the torn apart door.",
+				index:4
+			}
+		]
+	},
+	{
+		//room 2
+		title:"Office Room",
+		text:"Inside you see a small terminal emitting a small hum, ",
+		choices:[
+			{
+				text:"Go back to the Cryogenics room.",
+				index:0
+			}
+		]
+	},
+		{
+			//room 3
+			title:'Batch 3-47B Room',
+			text:"Room 4  ",
+			choices:[
+				{
+					text:"Go back to the hallway",
+					index:1
+				},
+				{
+					text:"Inspect the pods",
+					index:5
+				}
+			]
+	},
+
+	{
+		//room 4
+		title:'Batch 3-47B Room',
+		text:"Room 4  ",
+		choices:[
+			{
+				text:"Go back to the hallway",
+				index:1
+			},
+			{
+				text:"Inspect the pods",
+				index:5
+			}
+		]
+},
+
+
+]
